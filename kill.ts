5e6c5ae784ca9e9588c27a2b7f83624bfbcb68c9
hugoalh <hugoalh@gmail.com>
2024-12-30 17:12:47 +0800

    Update `killProcess`

--- kill.ts
+++ kill.ts
@@ -7,9 +7,21 @@ import {
 export type {
 	ProcessGetInfoOptions
 };
-export async function killProcess(input: number | string, options?: ProcessGetInfoOptions): Promise<void>;
-export async function killProcess(inputs: readonly (number | string)[], options?: ProcessGetInfoOptions): Promise<void>;
-export async function killProcess(param0: number | string | readonly (number | string)[], options?: ProcessGetInfoOptions): Promise<void> {
+/**
+ * Force end of the process, asynchronously.
+ * @param {number | string} input ID or name of the process.
+ * @param {Omit<ProcessGetInfoOptions, "depth">} [options={}] Options.
+ * @returns {Promise<void>}
+ */
+export async function killProcess(input: number | string, options?: Omit<ProcessGetInfoOptions, "depth">): Promise<void>;
+/**
+ * Force end of the processes, asynchronously.
+ * @param {readonly (number | string)[]} inputs ID or name of the processes.
+ * @param {Omit<ProcessGetInfoOptions, "depth">} [options={}] Options.
+ * @returns {Promise<void>}
+ */
+export async function killProcess(inputs: readonly (number | string)[], options?: Omit<ProcessGetInfoOptions, "depth">): Promise<void>;
+export async function killProcess(param0: number | string | readonly (number | string)[], options?: Omit<ProcessGetInfoOptions, "depth">): Promise<void> {
 	const fails: Error[] = [];
 	for (const input of (Array.isArray(param0) ? param0 : [param0])) {
 		if (typeof input === "number") {
@@ -21,7 +33,10 @@ export async function killProcess(param0: number | string | readonly (number | s
 		} else if (typeof input === "string") {
 			let processes: ProcessInfo[] = [];
 			try {
-				processes = (await getProcessInfo(input, options)).results;
+				processes = (await getProcessInfo(input, {
+					...options,
+					depth: 0
+				})).results;
 			} catch (error) {
 				fails.push(error as Error);
 			}
@@ -40,9 +55,21 @@ export async function killProcess(param0: number | string | readonly (number | s
 		throw new AggregateError(fails, `Unable to kill some of the processes!`);
 	}
 }
-export function killProcessSync(input: number | string, options?: ProcessGetInfoOptions): void;
-export function killProcessSync(inputs: readonly (number | string)[], options?: ProcessGetInfoOptions): void;
-export function killProcessSync(param0: number | string | readonly (number | string)[], options?: ProcessGetInfoOptions): void {
+/**
+ * Force end of the process, synchronously.
+ * @param {number | string} input ID or name of the process.
+ * @param {Omit<ProcessGetInfoOptions, "depth">} [options={}] Options.
+ * @returns {void}
+ */
+export function killProcessSync(input: number | string, options?: Omit<ProcessGetInfoOptions, "depth">): void;
+/**
+ * Force end of the processes, synchronously.
+ * @param {readonly (number | string)[]} inputs ID or name of the processes.
+ * @param {Omit<ProcessGetInfoOptions, "depth">} [options={}] Options.
+ * @returns {void}
+ */
+export function killProcessSync(inputs: readonly (number | string)[], options?: Omit<ProcessGetInfoOptions, "depth">): void;
+export function killProcessSync(param0: number | string | readonly (number | string)[], options?: Omit<ProcessGetInfoOptions, "depth">): void {
 	const fails: Error[] = [];
 	for (const input of (Array.isArray(param0) ? param0 : [param0])) {
 		if (typeof input === "number") {
@@ -54,7 +81,10 @@ export function killProcessSync(param0: number | string | readonly (number | str
 		} else if (typeof input === "string") {
 			let processes: ProcessInfo[] = [];
 			try {
-				processes = getProcessInfoSync(input, options).results;
+				processes = getProcessInfoSync(input, {
+					...options,
+					depth: 0
+				}).results;
 			} catch (error) {
 				fails.push(error as Error);
 			}
